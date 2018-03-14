1ecc9a5a11d30cc787565e357cc5309d7efaaea0
Lalitanand Dandge <dandgelb@gmail.com>
2018-03-14 12:16:49 +0530

    Spring Initializr created project
    Gradle, Java, Spring Boot 2.0.1 SNAPSHOT
    Group: my.local.examples
    Artifact: spring-starter-template

---
+++ src/main/java/my/local/examples/springstartertemplate/SpringStarterTemplateApplication.java
@@ -0,0 +1,12 @@
+package my.local.examples.springstartertemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStarterTemplateApplication.class, args);
+	}
+}
